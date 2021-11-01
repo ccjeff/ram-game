141a61b3d8a492fb6125f60b05411f72acdd4875
Brad Zhang <bwzhang@andrew.cmu.edu>
2021-11-01 19:53:35 -0400

    preliminary sprite layout

---
+++ Sprites.hpp
@@ -0,0 +1,23 @@
+#include <glm/glm.hpp>
+
+#include <vector>
+
+struct Transform{
+    glm::vec2 displacement;
+    // Counterclockwise rotation in radians
+    float rotation;
+}
+
+struct Picture{
+    std::vector< glm::u8vec4 > &data;
+    glm::uvec2 size;
+}
+
+struct Sprite {
+    //indicates a transform w.r.t. the containing class
+    Transform transform;
+    Picture picture;
+    glm::u8vec4 tint;
+
+    void draw();
+};
