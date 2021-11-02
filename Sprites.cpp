807022b3acd751d009cd83b8d4e1c2a556531a7d
Brad Zhang <bwzhang@andrew.cmu.edu>
2021-11-01 22:03:13 -0400

    added preliminary draw() code

--- Sprites.cpp
+++ Sprites.cpp
@@ -1,3 +1,22 @@
 #include <Sprites.hpp>
 
-void Sprite::draw(){}
+Sprite::Sprite(std::vector< glm::u8vec4 > data, glm::uvec2 size, Transform transform={{0.f,0.f},0.f}){
+    this->transform = transform;
+    this->picture.data = data;
+    this->picture.size = size;
+    this->tint = {0,0,0,0};
+    glGenTextures(1, &tex);
+}
+
+Sprite::~Sprite(){
+    glDeleteTextures(1, &tex);
+}
+
+void Sprite::draw(){
+    //TODO: add transforms. Need player location/parent transform in order to do this.
+    //TODO: add tinting
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, picture.size.x, picture.size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, &picture.data);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
--- Sprites.hpp
+++ Sprites.hpp
@@ -1,4 +1,5 @@
 #include <glm/glm.hpp>
+#include "GL.hpp"
 
 #include <vector>
 
@@ -14,10 +15,13 @@ struct Picture{
 }
 
 struct Sprite {
+    Sprite(std::vector< glm::u8vec4 > data, glm::uvec2 size, Transform transform={{0.f,0.f},0.f});
+    ~Sprite();
+    GLuint tex = 0;
     //indicates a transform w.r.t. the containing class
     Transform transform;
     Picture picture;
-    glm::u8vec4 tint;
+    glm::uvec4 tint;
 
     void draw();
 };
