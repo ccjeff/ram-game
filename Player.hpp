fddf15c4cd2bdd66e4e30d3917811e0f708a9935
ccjeff <jefferycao.czj@gmail.com>
2021-11-01 23:13:54 -0400

    update player and sprites

--- Player.hpp
+++ Player.hpp
@@ -4,9 +4,8 @@
 
 class Player {
 public:
-	Player() = default;
     ~Player() = default;
-    Player(glm::vec2 position, glm::vec2 rotation) : position(position), rotation(rotation) {};
+    Player(glm::vec2 position, glm::vec2 velocity) : position(position), velocity(velocity) {};
     void move(float elapsed);
     void move(glm::vec2 direction, float elapsed);
     void update(float elapsed);
@@ -14,7 +13,5 @@ public:
 protected:
 	glm::vec2 position; // should init to a location
     glm::vec2 velocity;
-    glm::vec2 acceleration;
     Sprite s;
-
 };
