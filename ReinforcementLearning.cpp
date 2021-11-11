50c92b0819cbecf055b15ee6579ffcc8cc4a504a
lishane31 <38923771+lishane31@users.noreply.github.com>
2021-11-11 11:30:25 -0500

    Create ReinforcementLearning.cpp

---
+++ ReinforcementLearning.cpp
@@ -0,0 +1,7 @@
+#include "ReinforcementLearning.hpp"
+
+ReinforcementLearning::ReinforcementLearning(std::shared_ptr<Player> player) : Items::player(player) {};
+
+void ReinforcementLearning::on_kill() {
+   
+}
